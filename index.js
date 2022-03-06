936fcaa89c61e9c24a78f06fd60338d1792171e6
Kent Roth <kentencroth@yahoo.com>
2022-03-06 15:22:23 -0600

    server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('./mongoose');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	next();
+});
+
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Server up on ${port}`);
+});
