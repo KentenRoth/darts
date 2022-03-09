ca4b0a493d9c4c8ebf9d857af9b42a9dcf1f2375
Kent Roth <kentencroth@yahoo.com>
2022-03-09 09:25:44 -0600

    Hashing password

--- darts/server/models/Users.js
+++ darts/server/models/Users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 // TODO
@@ -41,6 +41,16 @@ const UserSchema = new Schema({
 			},
 		},
 	],
+});
+
+UserSchema.pre('save', async function (next) {
+	const user = this;
+
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+
+	next();
 });
 
 module.exports = User = mongoose.model('User', UserSchema);
