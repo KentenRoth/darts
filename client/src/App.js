7770cfbc57a9972f91fff964bef18509f7d86824
Kent Roth <kentencroth@yahoo.com>
2022-03-26 17:30:21 -0500

    Setting up frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Sidebar from './Components/sidebar';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Sidebar />
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
---
+++ client/src/Components/sidebar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	// TODO:
+	// Props:
+	// Current User Name
+	// Current User Stats
+	// Friends List
+	// Function to send added friend to parent
+
+	// Input status data
+	// Map over friends
+	// Add friend to game board
+
+	render() {
+		return (
+			<div className="sideBar">
+				<div className="sideBar_heading">
+					<h1>Kent</h1>
+				</div>
+				<div className="sideBar_stats">
+					<h2 className="sideBar_stats__title">Stats</h2>
+					<ul>
+						<li>Highest Score: 151</li>
+						<li>Highest Out: 94</li>
+						<li>10 Match AVG: 47.33</li>
+					</ul>
+				</div>
+				<div className="sideBar_friends">
+					<h2 className="sideBar_friends__title">Friends</h2>
+					<ul>
+						<li>
+							Friend One
+							<button>+</button>
+						</li>
+						<li>
+							Friend Two <button>+</button>
+						</li>
+						<li>
+							Friend Three <button>+</button>
+						</li>
+						<li>
+							Friend Four <button>+</button>
+						</li>
+						<li>
+							Friend Five <button>+</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Sidebar;
